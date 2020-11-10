e4b6da173a6cabacf4cd1b9be53119b2be521683
Daniela Vieira <danielavieira2024@gmail.com>
2020-11-10 09:00:28 -0300

    Python
    #Criar variáveis com todas as disciplina de seu curso em seguida mostrar as notas  de cada disciplina e a soma de total das notas, por fim, calcula a média geral.

--- Primeiro programa em python.py
+++ Primeiro programa em python.py
@@ -1,6 +1,10 @@
-# Criar variavéis com todas as diciplina de seu curso em seguida mostrar as notas  de cada
-#diciplina e  depois calcula a média geral.
+#Criar variáveis com todas as disciplina de seu curso em seguida mostrar as notas  de cada disciplina e a soma de total das notas, por fim, calcula a média geral.
 
+
+
+
+
+soma= 0
 media = 0
 Inovacao= float (input ('Qual sua nota em Inovação?  '))
 Programacao = float (input('Qual sua nota em Programação?  '))
@@ -8,8 +12,9 @@ Rede= float (input ('Qual a sua nota em Rede?  '))
 Informatica= float (input ('Qual sua nota em Informática?  '))
 Estrutura=  float (input ('Qual sua nota em Estruta?  '))
 
-media = ( Inovacao + Programacao + Rede + Informatica + Estrutura) / 5
+soma = (soma + Inovacao + Programacao + Rede + Informatica + Estrutura) 
+media = soma / 5 
 
 print ('Sua nota em Inovação é:  ', Inovacao, ', sua nota em Programação é:  ', Programacao,
        ', sua  nota em Rede é:  ' , Rede, ', sua nota em Informática é:  ', Informatica,
-       ', sua nota em Estrutura é:  ' , Estrutura, '  ,   A média geral é:   ' , media)
+       ', sua nota em Estrutura é:  ' , Estrutura,  ', a soma de todas as minhas disciplina é:   '  , soma,   ',  A média geral é:   ' , media)
