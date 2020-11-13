c2789e73984326b26db5d6f3beccf2d0247d3996
Daniela Vieira <danielavieira2024@gmail.com>
2020-11-13 16:01:44 -0300

    python

---
+++ 12.py
@@ -0,0 +1,19 @@
+a = str (input('Qual a disciplina  1: '))
+b = str (input ('Qual a disciplina 2: '))
+c = str (input ('Qual a disciplina 3: '))
+
+print ( "\nMinha disciplina1: ", a ,",",b, " e",c, "\n" )
+
+
+m = 0
+n1 = float (input( "A minha nota em      " + a + "   é?   "))
+n2 = float (input("A minha nota em    "+ b+ "   é?     "))
+n3 = float (input ("A  minha nota em " +c+ "   é?    "))
+
+m = (n1 + n2 + n3) / 3
+
+print ("\n Minha nota em   " + a +  "   é   ", n1, "\n minha nota em  " +b+  "  é ", n2, "  \nminha nota  em   "+c+ " é " , n3, "\n" "\n é a média geral é: ", m)
+
+
+
+
